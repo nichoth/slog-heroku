5113cc884c5c4fca9278e9ecfde634d502d3612e
nichoth <nichoth@gmail.com>
2016-02-03 12:36:09 -0800

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var http = require('http');
+var data = require('./data.json');
 
-var URI = process.env.MONGO_URI;
+var URI = process.env.MONGOLAB_URI;
+
+console.log(URI);
 
 http.createServer(function(req, resp) {
   resp.setHeader('Content-Type', 'text/html');
+  resp.write(URI || 'bla');
   resp.end('hello');
 }).listen(process.env.PORT || 8000);
