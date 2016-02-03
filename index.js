f462bb743b1f82ca9ea82b6d95c1169c71212c0b
nichoth <nichoth@gmail.com>
2016-02-03 12:47:02 -0800

    nodemon for local heroku

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var data = require('./data.json');
 
 var URI = process.env.MONGOLAB_URI;
 
-console.log(URI);
-
 http.createServer(function(req, resp) {
   resp.setHeader('Content-Type', 'text/html');
+  resp.write('<pre>');
   resp.write(URI || 'bla');
-  resp.end('hello');
+  resp.write('\nhello');
+  resp.end('</pre>');
 }).listen(process.env.PORT || 8000);
